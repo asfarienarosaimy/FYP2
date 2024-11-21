277928aeb78cdfef9ac0c3fd625a736d0e36cc18
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2024-11-21 13:59:04 +0800

    Update loaneligibility.py

--- loaneligibility.py
+++ loaneligibility.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 import streamlit.io
 
 test = pd.read_csv('https://raw.githubusercontent.com/asfarienarosaimy/FYP2/refs/heads/main/testing.csv')
