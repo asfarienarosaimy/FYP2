e4041f4e576f22f46f1a575f88d066c909d60f83
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2024-12-10 00:07:09 +0800

    Update loaneligibility.py

--- loaneligibility.py
+++ loaneligibility.py
@@ -12,37 +12,4 @@ test_original = test.copy()
 
 st.write(train.head(3))
 
-st.write(train.info())
-
-st.write(test.info())
-
-st.write(train['Loan_Status'].value_counts())
-
-st.write(train['Loan_Status'].value_counts().plot.bar(figsize=(10,6), title='Loan Status'))
-
-plt.figure(1)
-plt.subplot(221)
-train['Gender'].value_counts(normalize=True).plot.bar(figsize=(20,10), title= 'Gender')
-plt.subplot(222)
-train['Married'].value_counts(normalize=True).plot.bar(title= 'Married')
-plt.subplot(223)
-train['Self_Employed'].value_counts(normalize=True).plot.bar(title= 'Self Employed')
-plt.subplot(224)
-train['Credit_History'].value_counts(normalize=True).plot.bar(title= 'Credit_History')
-
-plt.figure(1)
-plt.subplot(131)
-train['Dependents'].value_counts(normalize=True).plot.bar(figsize=(24,6), title='Dependents')
-plt.subplot(132)
-train['Education'].value_counts(normalize=True).plot.bar(title='Education')
-plt.subplot(133)
-train['Property_Area'].value_counts(normalize=True).plot.bar(title='Property Area')
-
-plt.figure(1)
-plt.subplot(121)
-sns.distplot(train['ApplicantIncome'])
-plt.subplot(122)
-train['ApplicantIncome'].plot.box(figsize=(16,5))
-
-train.boxplot(column= 'ApplicantIncome', by='Education')
-plt.tight_layout()
+st.write(test.head(3))
