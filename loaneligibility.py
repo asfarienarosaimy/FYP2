0ae059228b1ab236ea89de5781aaab0ce877a974
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2024-12-19 00:36:34 +0800

    Update loaneligibility.py

--- loaneligibility.py
+++ loaneligibility.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
