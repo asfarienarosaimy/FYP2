882d23915a46259372afe7214650937ea0af4b5a
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2024-12-26 16:38:27 +0800

    Update loaneligibility.py

--- loaneligibility.py
+++ loaneligibility.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
