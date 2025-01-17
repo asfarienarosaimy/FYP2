db690043497af8bb1b442841c2dabb182611851d
asfarienarosaimy <94262375+asfarienarosaimy@users.noreply.github.com>
2025-01-17 08:58:26 +0000

    update

--- prediction.py
+++ prediction.py
@@ -5,3 +5,11 @@ st.caption('prediction using supervised learning rf model')
 
 with st.sidebar:
     st.markdown('Menu Navigation')
+    eda = st.Page('loaneligibility.py', title='EDA', icon='📊')
+    predict = st.Page('prediction.py', title='Prediction Loan', icon='')
+
+
+
+
+
+
